87b18595d1b7e2c6ecba105ee2922b22ed331a44
leduong290300 <leduong290300@gmail.com>
2021-12-09 21:17:46 +0700

    Design

---
+++ resource/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import SelectCountries from "./components/SelectCountries.jsx";
+import Cards from "./components/Cards.jsx";
+import Charts from "./components/Charts.jsx";
+function App() {
+  return (
+    <>
+      <SelectCountries />
+      <Cards />
+      <Charts />
+    </>
+  );
+}
+
+export default App;
