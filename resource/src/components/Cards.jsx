fe4b5fb88886acdb2d0f0517114565e77a24e22c
leduong290300 <leduong290300@gmail.com>
2021-12-10 13:36:06 +0700

    select-country

--- resource/src/components/Cards.jsx
+++ resource/src/components/Cards.jsx
@@ -1,7 +1,34 @@
 import React from "react";
+import { Grid } from "@material-ui/core";
+import CartSingle from "./CardsSingle.jsx";
+function Cards({ reports }) {
+  const value = reports && reports.length ? reports[reports.length - 1] : [];
+  const report = [
+    {
+      title: "Số ca mắc",
+      count: value.Confirmed,
+      type: "confirmed",
+    },
+    {
+      title: "Số ca khỏi",
+      count: value.Recovered,
+      type: "recovered",
+    },
+    { title: "Số ca tử vong", count: value.Deaths, type: "deaths" },
+  ];
 
-function Cards() {
-  return <div></div>;
+  return (
+    <Grid container spacing={3}>
+      {report.map((item) => (
+        <CartSingle
+          title={item.title}
+          count={item.count}
+          type={item.type}
+          key={item.type}
+        />
+      ))}
+    </Grid>
+  );
 }
 
 export default Cards;
